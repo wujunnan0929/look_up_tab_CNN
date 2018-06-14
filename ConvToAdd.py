d9b79665c9c91146cc984703c78876b64c99fb82
wujunnan <wujunnan@sensetime.com>
2018-06-14 18:33:27 +0800

    update paper

--- ConvToAdd.py
+++ ConvToAdd.py
@@ -3,7 +3,23 @@ import torch.nn as nn
 import torch.nn.functional as Function
 import numpy as np
 
-class conv2add(Function):
+def r_to_e(r,N,tab):
+        for i in range(1,N+2):
+                if (r>tab[i]):
+                        return tab[i-1]
+
+def tab_init(a = 1.4, delta = 1e-4):
+	a=1.2
+	delta = 1e-4
+	N=math.ceil(abs(math.log(delta,a)))
+	tab=[]
+	for i in range(0,N+1):
+		tab.append(pow(a,-i))
+	tab[-1]=0
+	return tab
+
+
+class Conv2add(Function):
     def forward(self,input,weight,output_channel,kernel,stride,padding):
         r_to_e(weight)
         r_to_e(input)
@@ -31,12 +47,74 @@ class conv2add(Function):
                     ic += 1
                     result[0].sum
                     out.append.result
-
         return out
 
 class Alexnet(nn.model):
-    def __init__(self,tab):
+    def __init__(self,,num_classes,tab):
         self.tab=tab
+	super(AlexNet,self).__init__()
+	self.feature = nn.Sequential(
+	nn.Conv2d(3, 64, kernel_size = 11, stride = 4, padding = 2)
+	nn.ReLU(inplace = True),
+	nn.MaxPool2d(kernel_size = 3, stride = 2),
+	nn.Conv2d(64, 192, kernel_size=5, padding = 2),
+	nn.ReLU(inplace=True),
+	nn.MaxPool2d(kernel_size = 3, stride = 2)
+	nn.Conv2d(192, 384, kernel_size = 3, padding = 1),
+	nn.ReLU(inplace = True),
+	nn.Conv2d(384, 256, kernel_size = 3, padding = 1),
+	nn.ReLU(inplace = True),
+	nn.Conv2d(256, 256, kernel_size = 3, padding = 1),
+	nn.ReLU(inplace = True),
+	nn.MaxPool2d(kernel_size = 3, stride = 2),	
+	)
+	self.classifier = nn.Sequential(
+	nn.Dropput(),
+	nn.Linear(256 * 6 * 6, 4096),
+	nn.ReLU(inplace=True),
+	nn.Dropout(),
+	nn.Linear(4096,4096),
+	nn.ReLU(inplace=True),
+	nn.Linear(4096, num_classes),	
+	)
 
-    def forward(self.input):
-        
+    def forward(self,input):
+	x = self.feature(input)
+	x = x.view(x.size(), 256*6*6)
+	x = self.classifier(x)
+	return x
+	        
+class MyAlexnet(nn.model):
+    def __init__(self,,num_classes,tab):
+        self.tab=tab
+	super(AlexNet,self).__init__()
+	self.feature = nn.Sequential(
+	nn.Conv2add(3, 64, kernel_size = 11, stride = 4, padding = 2)
+	nn.ReLU(inplace = True),
+	nn.MaxPool2d(kernel_size = 3, stride = 2),
+	nn.Conv2add(64, 192, kernel_size=5, padding = 2),
+	nn.ReLU(inplace=True),
+	nn.MaxPool2d(kernel_size = 3, stride = 2)
+	nn.Conv2add(192, 384, kernel_size = 3, padding = 1),
+	nn.ReLU(inplace = True),
+	nn.Conv2add(384, 256, kernel_size = 3, padding = 1),
+	nn.ReLU(inplace = True),
+	nn.Conv2add(256, 256, kernel_size = 3, padding = 1),
+	nn.ReLU(inplace = True),
+	nn.MaxPool2d(kernel_size = 3, stride = 2),	
+	)
+	self.classifier = nn.Sequential(
+	nn.Dropput(),
+	nn.Linear(256 * 6 * 6, 4096),
+	nn.ReLU(inplace=True),
+	nn.Dropout(),
+	nn.Linear(4096,4096),
+	nn.ReLU(inplace=True),
+	nn.Linear(4096, num_classes),	
+	)
+
+    def forward(self,input):
+	x = self.feature(input)
+	x = x.view(x.size(), 256*6*6)
+	x = self.classifier(x)
+	return x
--- inference_time/conv.py
+++ inference_time/conv.py
@@ -4,78 +4,122 @@ import scipy.signal
 import math
 import torch
 import torch.nn as nn
+from bisect import bisect_left
 
-def r_to_e(r,N,tab):
-	for i in range(1,N+1):
-		if (r<tab[i]):
-			return tab[i-1]
+def r_to_e(r,tab):
+	low = 0
+	high = len(tab)-1
+	while low<high:
+		mid = int((low+high)/2)
+		#print("%d:%d:%d"%(low,mid,high))
+		if (r<tab[mid]):
+			low = mid+1
+		else:
+			high = mid-1
+	return tab[mid]
+def r_to_e_lib(r,tab):
+	i = bisect_left(tab, r)
+	j = np.searchsorted(tab, r)
+	print("lib i:",i,j)
+	if(i>=0):
+		return tab[i-1]
+	else:
+		return tab[0]
+
+def r_to_e_fake(r,tab):
+	return tab[1]
+
+def get_tab(a=1.2,delta=1e-3):
+	N=math.ceil(abs(math.log(delta,a)))
+	print("N:",N)
+	tab=[]
+	for i in range(0,N+1):
+		tab.append(pow(a,-i))
+	tab[-1]=0
+	print("tab size:",len(tab))
+	print("tab:\n",tab)
+	return tab
 
 a=1.2
-delta = 1e-4
-N=math.ceil(abs(math.log(delta,a)))
-tab=[]
-for i in range(0,N+1):
-	tab.append(pow(a,-i))
-tab[-1]=0
-print("tab:\n",tab)
-
+delta=1e-2
+tab = get_tab(a,delta)
+#print("e:",r_to_e(0.5,tab),r_to_e_lib(0.5,tab))
+loop=100
+start = time.time()
+for i in range(0,loop):
+	e = r_to_e(0.5,tab)
+end = time.time()
+t0 = (end-start)
+print("r_to_e used time %f:%f s"%(t0,t0/loop))
+w=3
+h=3
 dim=3
 data_in = np.random.random((dim,dim))
-p_data_in = np.random.random((dim,dim,dim))
+b_data_in = np.random.random((w,h))
 print("in:\n",data_in)
 kernel = np.random.random((dim,dim))
 print("kernel:\n",kernel)
 
+n = 100
 ##convolution
-n = 10000
 start = time.time()
 for i in range(0,n):
-	out = scipy.signal.convolve2d(data_in,kernel)
+	out = scipy.signal.convolve2d(b_data_in,kernel)
 end = time.time()
-t1 = end -start
+t1 = (end -start)/n
 print("out:\n",out)
-print("%d conv used %f s"%(n,t1))
+print("=====================================================\n")
+print("n = %d, scipy conv used %f s"%(n,t1))
+print("=====================================================\n")
 
 ##pytorch convolution
-n = 10000
-tensor = torch.FloatTensor(data_in)
+tensor = torch.FloatTensor(b_data_in)
 tensor = tensor.unsqueeze_(0)
 tensor = tensor.unsqueeze_(0)
-print("tensor:",tensor)
 print("tensor shape:",tensor.size())
+print("tensor:\n",tensor)
 conv1 = nn.Conv2d(1,1,dim,stride=1)
 start = time.time()
 for i in range(0,n):
 	#conv = torch.nn.Conv2d(in_channels=1,out_channels=1,kernel_size=(dim,dim),stride=1,padding=0,dilation=1,groups=1,bias=False)
 	out = conv1(tensor)
 end = time.time()
-t2 = end -start
-print("out:\n",out)
-print("%d pytorch conv used %f s"%(n,t2))
+t2 = (end -start)/n
+print("out:\n",out,out.size())
+print("=====================================================\n")
+print("n = %d, pytorch conv used %f s"%(n,t2))
+print("=====================================================\n")
 
 ##look up table: real to element
 for element in data_in.flat:
-	element = r_to_e(element, N, tab)
+	element = r_to_e(element, tab)
+	print("element in date_in:",element)
 
 print("r_to_e data_in\n",data_in)
 
 for element in kernel.flat:
-	element = r_to_e(element, N, tab)
+	element = r_to_e(element, tab)
 
 print("r_to_e kenel\n",kernel)
 
 ##add
 start = time.time()
 for i in range(0,n):
-	tmp = data_in
-	data_in = kernel
-	kernel = tmp 
-	out = data_in+kernel
-result = np.sum(out)
+	for element in b_data_in.flat:
+		element = r_to_e(element, tab)
+		#element = r_to_e_fake(element, tab)
+		#element = 1
+	for j in range(0, w-dim+1):
+		for j in range(0, h-dim+1):
+			out = data_in+kernel
+			result = np.sum(out)
 end = time.time()
-t3 = end -start
+t3 = (end -start)/n
 print("out:\n",out)
 print("result:\n",result)
-print("%d look up table add used %f s, speed up %f:%f times"%(n,t3,t1/t3,t2/t3))
-
-
+print("=====================================================\n")
+print("n = %d, scipy conv used %f s"%(n,t1))
+print("n = %d, pytorch conv used %f s"%(n,t2))
+print("n = %d, look up table add used %f s"%(n,t3))
+print("tab len:%d,speed up %f:%f times"%(len(tab),t1/t3,t2/t3))
+print("=====================================================\n")
